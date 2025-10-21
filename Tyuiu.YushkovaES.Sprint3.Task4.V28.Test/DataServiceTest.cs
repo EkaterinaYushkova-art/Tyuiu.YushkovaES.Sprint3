431044b20b81a6243a3e8122837a94af7370b566
Юшкова Екатерина <yekaterina.yushkova.07@mail.ru>
2025-10-21 14:15:31 +0300

    Task4

---
+++ Tyuiu.YushkovaES.Sprint3.Task4.V28.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.YushkovaES.Sprint3.Task4.V28.Lib;
+namespace Tyuiu.YushkovaES.Sprint3.Task4.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 1.924;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.YushkovaES.Sprint3.Task4.V28/Program.cs
@@ -0,0 +1,55 @@
+﻿using Tyuiu.YushkovaES.Sprint3.Task4.V28.Lib;
+namespace Tyuiu.YushkovaES.Sprint3.Task4.V28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Екатерина Е.С | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Оператор цикла while                                             *");
+            Console.WriteLine("* Задание #4                                                             *");
+            Console.WriteLine("* Вариант #28                                                            *");
+            Console.WriteLine("* Выполнила: Екатерина Е.С | ИСПб-25-1                                   *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Используя цикл foreach заменить буквы q на цифру 7 в строке            *");
+            Console.WriteLine("* sqwreq vqwqq q                                                         *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Исходные данные:");
+            Console.WriteLine("Старт диапазона: " + startValue);
+            Console.WriteLine("Конец диапазона: " + stopValue);
+
+
+
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            double result = ds.Calculate(startValue, stopValue);
+            Console.WriteLine("Сумма значений функции: " + result);
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
