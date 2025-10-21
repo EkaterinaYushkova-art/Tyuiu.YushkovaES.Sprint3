ecc63cfe63b920c6a24a26b27ef234453426a81a
Юшкова Екатерина <yekaterina.yushkova.07@mail.ru>
2025-10-21 14:34:32 +0300

    Task5

---
+++ Tyuiu.YushkovaES.Sprint3.Task5.V26.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.YushkovaES.Sprint3.Task5.V26.Lib
+{
+    public class DataService : ISprint3Task5V26
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sum = 0;
+
+            for (int i = startValue1; i <= stopValue1; i++)
+            {
+                for (int k = startValue2; k <= stopValue2; k++)
+                {
+                    double y = (Math.Cos(x) + Math.Sin(k)) / x;
+                    sum += y;
+                }
+            }
+
+            return Math.Round(sum, 3);
+
+        }
+    }
+}
---
+++ Tyuiu.YushkovaES.Sprint3.Task5.V26/Program.cs
@@ -0,0 +1,59 @@
+﻿using Tyuiu.YushkovaES.Sprint3.Task5.V26.Lib;
+namespace Tyuiu.YushkovaES.Sprint3.Task5.V26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Екатерина Е.С | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                  *");
+            Console.WriteLine("* Задание #5                                                             *");
+            Console.WriteLine("* Вариант #26                                                            *");
+            Console.WriteLine("* Выполнила: Екатерина Е.С | ИСПб-25-1                                   *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Вычислите с помощью вложенных циклов выражение при x = 2               *");
+            Console.WriteLine("*   3   13                                                               *");
+            Console.WriteLine("*   ∑   ∑  (x^k + sin(k))                                                *");
+            Console.WriteLine("*  i=1 k=1                                                               *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+
+            int x = 2;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 3;
+
+            Console.WriteLine("Исходные данные:");
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Старт внешнего цикла: " + startValue1);
+            Console.WriteLine("Конец внешнего цикла: " + stopValue1);
+            Console.WriteLine("Старт внутреннего цикла: " + startValue2);
+            Console.WriteLine("Конец внутреннего цикла: " + stopValue2);
+
+
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            double result = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+            Console.WriteLine("Сумма = " + result);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
