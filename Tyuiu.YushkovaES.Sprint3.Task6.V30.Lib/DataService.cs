541c30f782a3b946aed96d0f830d7a75c5c37aca
Юшкова Екатерина <yekaterina.yushkova.07@mail.ru>
2025-10-22 16:46:04 +0300

    Task6

---
+++ Tyuiu.YushkovaES.Sprint3.Task6.V30.Lib/DataService.cs
@@ -0,0 +1,30 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.YushkovaES.Sprint3.Task6.V30.Lib
+{
+    public class DataService : ISprint3Task6V30
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int totalSum = 0;
+
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                int sumDivisors = 0;
+
+                // Находим все делители числа x
+                for (int i = 1; i <= x; i++)
+                {
+                    if (x % i == 0)
+                    {
+                        sumDivisors += i;
+                    }
+                }
+
+                totalSum += sumDivisors;
+            }
+
+            return totalSum;
+        }
+    }
+}
---
+++ Tyuiu.YushkovaES.Sprint3.Task6.V30/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.YushkovaES.Sprint3.Task6.V30.Lib;
+namespace Tyuiu.YushkovaES.Sprint3.Task6.V30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнила: Екатерина Е.С | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #3                                                              *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                  *");
+            Console.WriteLine("* Задание #6                                                             *");
+            Console.WriteLine("* Вариант #30                                                            *");
+            Console.WriteLine("* Выполнила: Екатерина Е.С | ИСПб-25-1                                   *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел,                    *");
+            Console.WriteLine("*  принадлежащих числовому отрезку [11, 17] количество всех делителей.   *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int startValue = 11;
+            int stopValue = 17;
+            Console.WriteLine($"Начало диапазона: {startValue}");
+            Console.WriteLine($"Конец диапазона: {stopValue}");
+
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+            int result = ds.GetSumTheDivisors(startValue, stopValue);
+            Console.WriteLine($"Сумма всех делителей чисел в диапазоне [{startValue}, {stopValue}] = {result}");
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
